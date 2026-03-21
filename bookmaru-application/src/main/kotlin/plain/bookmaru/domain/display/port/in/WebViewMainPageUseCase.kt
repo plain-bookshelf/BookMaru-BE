b75eb6df6a39d17dde27b87d8169d93ae9dee125
Heiji57 <kjone0kjone0591@gmail.com>
2026-03-22 06:06:25 +0900

    refactor: WebViewMainPageUseCase delete - WebViewMainPageUseCase 삭제

--- bookmaru-application/src/main/kotlin/plain/bookmaru/domain/display/port/in/WebViewMainPageUseCase.kt
+++
@@ -1,9 +0,0 @@
-package plain.bookmaru.domain.display.port.`in`
-
-import plain.bookmaru.common.command.PageCommand
-import plain.bookmaru.domain.auth.vo.PlatformType
-import plain.bookmaru.domain.display.port.out.result.ViewMainPageEventResult
-
-interface WebViewMainPageUseCase {
-    suspend fun webExecute(command: PageCommand, platformType: PlatformType, affiliationId: Long) : ViewMainPageEventResult
-}
