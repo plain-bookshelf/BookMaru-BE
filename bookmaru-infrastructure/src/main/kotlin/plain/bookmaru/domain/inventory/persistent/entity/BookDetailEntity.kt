52367689784929e0a3a84293661f1dab8f67dc37
Heiji57 <kjone0kjone0591@gmail.com>
2026-04-12 08:29:17 +0900

    fix: bookDetailEntity - bookDetailEntity rentalStatus 타입 수정

--- bookmaru-infrastructure/src/main/kotlin/plain/bookmaru/domain/inventory/persistent/entity/BookDetailEntity.kt
+++ bookmaru-infrastructure/src/main/kotlin/plain/bookmaru/domain/inventory/persistent/entity/BookDetailEntity.kt
@@ -2,6 +2,8 @@ package plain.bookmaru.domain.inventory.persistent.entity
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -55,6 +57,7 @@ class BookDetailEntity(
     @JoinColumn(name = "member_id")
     var memberEntity: MemberEntity? = null
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     var rentalStatus : RentalStatus = RentalStatus.RETURN
 
