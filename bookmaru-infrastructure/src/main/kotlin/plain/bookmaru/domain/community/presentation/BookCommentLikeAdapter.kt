fbe8e6ae401eca19d483c960479ad6c154a9ae87
Heiji57 <kjone0kjone0591@gmail.com>
2026-04-12 10:58:29 +0900

    refactor: Dockerfile - Dockerfile 수정

--- bookmaru-infrastructure/src/main/kotlin/plain/bookmaru/domain/community/presentation/BookCommentLikeAdapter.kt
+++ bookmaru-infrastructure/src/main/kotlin/plain/bookmaru/domain/community/presentation/BookCommentLikeAdapter.kt
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import plain.bookmaru.common.annotation.LogExecution
@@ -23,7 +23,7 @@ class BookCommentLikeAdapter(
     private val commentUnLikeUseCase: CommentUnLikeUseCase
 ) {
 
-    @GetMapping("/like")
+    @PostMapping("/like")
     @LogExecution
     suspend fun commentLike(
         @PathVariable commentId: Long,
