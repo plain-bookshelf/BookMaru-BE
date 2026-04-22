3de47d51150804c536313b19c51d5ed43a2f2c0e
Heiji57 <kjone0kjone0591@gmail.com>
2026-04-23 00:16:54 +0900

    fix: ranking - 랭킹 로직 수정

--- bookmaru-application/src/main/kotlin/plain/bookmaru/domain/display/port/in/ViewRankingPageUseCase.kt
+++ bookmaru-application/src/main/kotlin/plain/bookmaru/domain/display/port/in/ViewRankingPageUseCase.kt
@@ -5,5 +5,5 @@ import plain.bookmaru.domain.display.port.`in`.command.ViewRankingPageCommand
 import plain.bookmaru.domain.display.port.out.result.UserRankInfoResult
 
 interface ViewRankingPageUseCase {
-    suspend fun execute(command: ViewRankingPageCommand) : SliceResult<UserRankInfoResult>
+    suspend fun execute(command: ViewRankingPageCommand) : List<UserRankInfoResult>
 }
--- bookmaru-application/src/main/kotlin/plain/bookmaru/domain/display/service/ViewRankingPageService.kt
+++ bookmaru-application/src/main/kotlin/plain/bookmaru/domain/display/service/ViewRankingPageService.kt
@@ -2,7 +2,6 @@ package plain.bookmaru.domain.display.service
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import plain.bookmaru.common.annotation.Service
-import plain.bookmaru.common.result.SliceResult
 import plain.bookmaru.domain.display.port.`in`.ViewRankingPageUseCase
 import plain.bookmaru.domain.display.port.`in`.command.ViewRankingPageCommand
 import plain.bookmaru.domain.display.port.out.RankingPagePort
@@ -14,16 +13,13 @@ private val log = KotlinLogging.logger {}
 class ViewRankingPageService(
     private val rankingPagePort: RankingPagePort
 ): ViewRankingPageUseCase {
-    override suspend fun execute(command: ViewRankingPageCommand): SliceResult<UserRankInfoResult> {
-        val cacheResult = rankingPagePort.loadRankingPage(command.pageCommand, command.affiliationId)
+    override suspend fun execute(command: ViewRankingPageCommand): List<UserRankInfoResult> {
+        val cacheResult = rankingPagePort.loadRankingPage(command.affiliationId)
 
         if (cacheResult != null) return cacheResult
 
         log.warn { "Ranking 캐시 miss 로 인해 빈 리스트를 반환합니다. (AffiliationId: ${command.affiliationId})" }
 
-        return SliceResult(
-            content = emptyList(),
-            isLastPage = true
-        )
+        return emptyList()
     }
 }
