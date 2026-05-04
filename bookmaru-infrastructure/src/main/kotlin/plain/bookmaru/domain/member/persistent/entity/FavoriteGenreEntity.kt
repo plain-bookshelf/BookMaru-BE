d3f73bb2792eacecb111381a2f37af877d56cccd
Heiji57 <kjone0kjone0591@gmail.com>
2026-05-04 15:51:55 +0900

    refactor: code - 코드 수정

--- bookmaru-infrastructure/src/main/kotlin/plain/bookmaru/domain/member/persistent/entity/FavoriteGenreEntity.kt
+++ bookmaru-infrastructure/src/main/kotlin/plain/bookmaru/domain/member/persistent/entity/FavoriteGenreEntity.kt
@@ -2,10 +2,12 @@ package plain.bookmaru.domain.member.persistent.entity
 
 import jakarta.persistence.EmbeddedId
 import jakarta.persistence.Entity
+import jakarta.persistence.Table
 import plain.bookmaru.domain.member.persistent.entity.embedded.FavoriteGenreEmbeddedId
 import plain.bookmaru.global.entity.BaseEntity
 
 @Entity
+@Table(name = "favorite_genre")
 class FavoriteGenreEntity(
     @EmbeddedId
     override val id: FavoriteGenreEmbeddedId
--- bookmaru-infrastructure/src/main/kotlin/plain/bookmaru/domain/member/persistent/entity/embedded/FavoriteGenreEmbeddedId.kt
+++ bookmaru-infrastructure/src/main/kotlin/plain/bookmaru/domain/member/persistent/entity/embedded/FavoriteGenreEmbeddedId.kt
@@ -1,9 +1,10 @@
 package plain.bookmaru.domain.member.persistent.entity.embedded
 
 import jakarta.persistence.Embeddable
+import java.io.Serializable
 
 @Embeddable
 data class FavoriteGenreEmbeddedId(
     val memberId: Long,
     val genreId: Long
-)
+) : Serializable
