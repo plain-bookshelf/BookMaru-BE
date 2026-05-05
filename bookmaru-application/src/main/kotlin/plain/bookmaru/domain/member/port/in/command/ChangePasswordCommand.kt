0e7be18cea2d5e0c2596fbadd36d8f5323219c51
Heiji57 <kjone0kjone0591@gmail.com>
2026-05-05 14:59:16 +0900

    refactor: code - 코드 수정

--- bookmaru-application/src/main/kotlin/plain/bookmaru/domain/member/port/in/command/ChangePasswordCommand.kt
+++ bookmaru-application/src/main/kotlin/plain/bookmaru/domain/member/port/in/command/ChangePasswordCommand.kt
@@ -8,7 +8,7 @@ data class ChangePasswordCommand(
 ) {
     init {
         require(newPassword.isNotEmpty())
-        require(newPassword.length < 20)
+        require(newPassword.length in 8 .. 20)
         require(newPassword.first() in 'a'..'z' || newPassword.first() in 'A'..'Z') { "비밀번호 첫째 자리는 영문자이어야 합니다." }
         require(newPassword.last() in 'a'..'z' || newPassword.last() in 'A'..'Z' || newPassword.last() in '0'..'9' || newPassword.last() == '!' || newPassword.last() == '~' || newPassword.last() == '#') { "비밀번호 마지막 자리는 영문자, 숫자, !,~,# 중 하나 이어야 합니다." }
     }
--- bookmaru-application/src/main/kotlin/plain/bookmaru/domain/verification/port/in/command/ResetPasswordCommand.kt
+++ bookmaru-application/src/main/kotlin/plain/bookmaru/domain/verification/port/in/command/ResetPasswordCommand.kt
@@ -4,4 +4,11 @@ data class ResetPasswordCommand(
     val email: String,
     val newPassword: String,
     val registerToken: String
-)
+) {
+    init {
+        require(newPassword.isNotEmpty())
+        require(newPassword.length in 8 .. 20)
+        require(newPassword.first() in 'a'..'z' || newPassword.first() in 'A'..'Z') { "비밀번호 첫째 자리는 영문자이어야 합니다." }
+        require(newPassword.last() in 'a'..'z' || newPassword.last() in 'A'..'Z' || newPassword.last() in '0'..'9' || newPassword.last() == '!' || newPassword.last() == '~' || newPassword.last() == '#') { "비밀번호 마지막 자리는 영문자, 숫자, !,~,# 중 하나 이어야 합니다." }
+    }
+}
